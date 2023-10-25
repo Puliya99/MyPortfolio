e1ce4c2538bf523a1b16bae94e6e2c4e64f7304f
lmarcho <lakshitha.mathngadeera@neosolax.com>
2023-10-25 22:13:01 +0530

    update portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+$("#nav-icon-open").on("click", () => {
+    $("#nav-icon-open").css("display", "none");
+    $("#nav-icon-close").css("display", "block");
+    $("#mob-nav").css("display", "block");
+});
+
+$("#nav-icon-close").on("click", () => {
+    $("#nav-icon-open").css("display", "block");
+    $("#nav-icon-close").css("display", "none");
+    $("#mob-nav").css("display", "none");
+});
+
+$("#homeLink").on("click", () => {
+    $("#nav-icon-open").css("display", "block");
+    $("#nav-icon-close").css("display", "none");
+    $("#mob-nav").css("display", "none")
+});
+
+$("#aboutLink").on("click", () => {
+    $("#nav-icon-open").css("display", "block");
+    $("#nav-icon-close").css("display", "none");
+    $("#mob-nav").css("display", "none")
+});
+
+$("#serviceLink").on("click", () => {
+    $("#nav-icon-open").css("display", "block");
+    $("#nav-icon-close").css("display", "none");
+    $("#mob-nav").css("display", "none")
+});
+
+$("#workLink").on("click", () => {
+    $("#nav-icon-open").css("display", "block");
+    $("#nav-icon-close").css("display", "none");
+    $("#mob-nav").css("display", "none")
+});
+
+$("#galleryLink").on("click", () => {
+    $("#nav-icon-open").css("display", "block");
+    $("#nav-icon-close").css("display", "none");
+    $("#mob-nav").css("display", "none")
+});
+
+$("#contactLink").on("click", () => {
+    $("#nav-icon-open").css("display", "block");
+    $("#nav-icon-close").css("display", "none");
+    $("#mob-nav").css("display", "none")
+});
